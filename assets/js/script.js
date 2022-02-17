4e4763ad38327cfa5f73aab1806be7b98fd2f8af
sighachandra <sighac@gmail.com>
2022-02-17 14:33:06 +0530

    Header and footer fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-cdscsdcf
+function handBurgerShow() {
+	console.log("hello");
+  document.getElementById("myDropdown").classList.toggle("show_h");
+}
+
+window.onclick = function(event) {
+  if (!event.target.parentNode.matches('.dropbtn_h') && !event.target.matches('.dropbtn_h')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content_h");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show_h')) {
+        openDropdown.classList.remove('show_h');
+      }
+    }
+  }
+}
